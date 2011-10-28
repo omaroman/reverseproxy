44ecf23a10105e49cc3ce4df56a1b9b03e29cb4b
Omar Román <omaroman@gmail.com>
2011-10-28 10:20:46 -0500

    forbidden http status code for Prohibited

--- app/controllers/reverseproxy/ProhibitedController.java
+++ app/controllers/reverseproxy/ProhibitedController.java
@@ -6,10 +6,13 @@
 package controllers.reverseproxy;
 
 import play.mvc.Controller;
+import play.mvc.Http;
 
 public class ProhibitedController extends Controller {
 
     public static void prohibited() {
+        //403 Forbidden
+        Http.Response.current().status = Http.StatusCode.FORBIDDEN;
         render();
     }
 }
